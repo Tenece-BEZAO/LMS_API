19877ea71b8924f4c21e9eaa3644b2c17809aff4
anya ebube <anyaebube4im@outlook.com>
2023-03-24 10:46:55 +0100

    Created branches in the DAL

---
+++ LMS/LMS.DAL/Repository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace AppDAL.Repository;
+
+
+public interface IUnitOfWork
+{
+    IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+    int SaveChanges();
+    void Dispose();
+    Task<int> SaveChangesAsync();
+}

c781b6bfb3bcf2e3336f8e6872af7ba1a60f7e96
john <chukwuchidieberejohn@gmail.com>
2023-03-27 13:23:05 +0100

    FEAT:
    added repository class

---
+++ LMS/LMS.DAL/Repository/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace LMS.DAL.Repository;
+
+public interface IUnitOfWork
+{
+    IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+    int SaveChanges();
+    void Dispose();
+    Task<int> SaveChangesAsync();
+}
